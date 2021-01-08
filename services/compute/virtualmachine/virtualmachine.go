94c4c220a27c0d592bcb85677173edbc89e1531f
JocelynBerrendonner <jocelynb@microsoft.com>
2021-01-08 14:10:51 -0800

    Fixing a regression

--- services/compute/virtualmachine/virtualmachine.go
+++ services/compute/virtualmachine/virtualmachine.go
@@ -218,7 +218,7 @@ func (c *client) getWssdVirtualMachineWindowsConfiguration(windowsConfiguration 
 		return wc
 	}
 
-	if windowsConfiguration.RDP != nil && windowsConfiguration.RDP.DisableRDP != nil {
+	if windowsConfiguration.RDP.DisableRDP != nil {
 		wc.RDPConfiguration.DisableRDP = *windowsConfiguration.RDP.DisableRDP
 	}
 
