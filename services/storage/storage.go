e44531b77e73dd95cd8a8f85e7621f7e69f5b9c3
Harika Vedati <havedati@microsoft.com>
2022-03-23 12:40:41 -0700

    updating possible values

--- services/storage/storage.go
+++ services/storage/storage.go
@@ -32,9 +32,9 @@ type VirtualHardDiskProperties struct {
 	Scsipath *string `json:"scsipath,omitempty"`
 	// State - State
 	Statuses map[string]*string `json:"statuses"`
-	//HyperVGeneration - Gets the HyperVGenerationType of the VirtualMachine created from the image.
+	//HyperVGeneration - Gets the HyperVGenerationType of the VirtualMachine created from the image. Possible values are common.HyperVGeneration_HyperVGenerationV1 and common.HyperVGeneration_HyperVGenerationV2  
 	HyperVGeneration common.HyperVGeneration `json:"hyperVGeneration,omitempty"`
-	//DiskFileFormat - File format of the disk
+	//DiskFileFormat - File format of the disk. possible values are common.DiskFileFormat_DiskFileFormatVHD and common.DiskFileFormat_DiskFileFormatVHDX
 	DiskFileFormat common.DiskFileFormat `json:"diskFileFormat,omitempty"`
 }
 
