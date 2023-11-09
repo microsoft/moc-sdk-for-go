3682657b5c348f7180e2bef9af97cfa7cecc259b
Dhwani Shah <dhwanishah@microsoft.com>
2023-11-09 10:57:54 -0800

    adding test cases

--- services/compute/virtualmachine/wssd_test.go
+++ services/compute/virtualmachine/wssd_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/microsoft/moc-sdk-for-go/services/compute"
 	wssdcloudproto "github.com/microsoft/moc/rpc/common"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_VirtualMachineValidations(t *testing.T) {
@@ -12,6 +13,7 @@ func Test_VirtualMachineValidations(t *testing.T) {
 	httpProxy := ""
 	httpsProxy := ""
 
+	// empty url
 	vm := &compute.VirtualMachine{
 		VirtualMachineProperties: &compute.VirtualMachineProperties{
 			OsProfile: &compute.OSProfile{
@@ -29,6 +31,7 @@ func Test_VirtualMachineValidations(t *testing.T) {
 		t.Fatalf("Test_VirtualMachineValidations failed: Error should be nil")
 	}
 
+	// Invalid url
 	httpProxy = "//skyproxy.ceccloud1.selfhost:3128"
 	vm = &compute.VirtualMachine{
 		VirtualMachineProperties: &compute.VirtualMachineProperties{
@@ -41,7 +44,23 @@ func Test_VirtualMachineValidations(t *testing.T) {
 		},
 	}
 	err = wssdcloudclient.virtualMachineValidations(wssdcloudproto.Operation_POST, vm)
-	if err == nil {
-		t.Fatalf("Test_VirtualMachineValidations failed. Expected test case to throw error: The URL scheme should be http or https: Invalid Input")
+	expectedErrorMsg := "Invalid proxy URL. The URL scheme should be http or https: Invalid Input"
+	assert.EqualErrorf(t, err, expectedErrorMsg, "Error should be: %v, got: %v", expectedErrorMsg, err)
+
+	//nil value
+	vm = &compute.VirtualMachine{
+		VirtualMachineProperties: &compute.VirtualMachineProperties{
+			OsProfile: &compute.OSProfile{
+				ProxyConfiguration: &compute.ProxyConfiguration{
+					HttpProxy:  nil,
+					HttpsProxy: nil,
+				},
+			},
+		},
+	}
+
+	err = wssdcloudclient.virtualMachineValidations(wssdcloudproto.Operation_POST, vm)
+	if err != nil {
+		t.Fatalf("Test_VirtualMachineValidations failed: Error should be nil for nil value")
 	}
 }
