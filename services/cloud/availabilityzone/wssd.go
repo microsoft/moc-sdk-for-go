7669950478ec8c1e37f6527a2ce1ed4f55f57574
MANISH KATTURU <mkatturu@microsoft.com>
2024-07-28 20:02:27 -0700

    added location check

--- services/cloud/availabilityzone/wssd.go
+++ services/cloud/availabilityzone/wssd.go
@@ -33,8 +33,8 @@ func newAvailabilityZoneClient(subID string, authorizer auth.Authorizer) (*clien
 }
 
 // Get
-func (c *client) Get(ctx context.Context, name string) (*[]cloud.AvailabilityZone, error) {
-	request, err := c.getAvailabilityZoneRequest(wssdcloudcommon.Operation_GET, name, nil)
+func (c *client) Get(ctx context.Context, location string, name string) (*[]cloud.AvailabilityZone, error) {
+	request, err := c.getAvailabilityZoneRequest(wssdcloudcommon.Operation_GET, location, name, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -47,8 +47,8 @@ func (c *client) Get(ctx context.Context, name string) (*[]cloud.AvailabilityZon
 }
 
 // Create
-func (c *client) CreateOrUpdate(ctx context.Context, name string, avzone *cloud.AvailabilityZone) (*cloud.AvailabilityZone, error) {
-	request, err := c.getAvailabilityZoneRequest(wssdcloudcommon.Operation_POST, name, avzone)
+func (c *client) CreateOrUpdate(ctx context.Context, location string, name string, avzone *cloud.AvailabilityZone) (*cloud.AvailabilityZone, error) {
+	request, err := c.getAvailabilityZoneRequest(wssdcloudcommon.Operation_POST, location, name, avzone)
 	if err != nil {
 		return nil, err
 	}
@@ -79,7 +79,7 @@ func (c *client) CreateOrUpdate(ctx context.Context, name string, avzone *cloud.
 }
 
 // Delete methods invokes create or update on the client
-func (c *client) Delete(ctx context.Context, name string) error {
+func (c *client) Delete(ctx context.Context, location string, name string) error {
 	vmss, err := c.Get(ctx, name)
 	if err != nil {
 		return err
@@ -88,7 +88,7 @@ func (c *client) Delete(ctx context.Context, name string) error {
 		return errors.NotFound
 	}
 
-	request, err := c.getAvailabilityZoneRequest(wssdcloudcommon.Operation_DELETE, name, &(*vmss)[0])
+	request, err := c.getAvailabilityZoneRequest(wssdcloudcommon.Operation_DELETE, location, name, &(*vmss)[0])
 	if err != nil {
 		return err
 	}
@@ -112,18 +112,19 @@ func (c *client) getAvailabilityZoneFromResponse(response *wssdcloudcompute.Avai
 
 }
 
-func (c *client) getAvailabilityZoneRequest(opType wssdcloudcommon.Operation, name string, avzone *cloud.AvailabilityZone) (*wssdcloudcompute.AvailabilityZoneRequest, error) {
+func (c *client) getAvailabilityZoneRequest(opType wssdcloudcommon.Operation, location string, name string, avzone *cloud.AvailabilityZone) (*wssdcloudcompute.AvailabilityZoneRequest, error) {
 	request := &wssdcloudcompute.AvailabilityZoneRequest{
 		OperationType:    opType,
 		AvailabilityZones: []*wssdcloudcompute.AvailabilityZone{},
 	}
 
-	if len(name) == 0 {
-		return nil, errors.Wrapf(errors.InvalidInput, "Name not specified")
+	if len(location) == 0 {
+		return nil, errors.Wrapf(errors.InvalidInput, "Location not specified")
 	}
 
 	avzoneRet := &wssdcloudcompute.AvailabilityZone{
 		Name:      name,
+		LocationName: location,
 	}
 
 	if avzone != nil {
