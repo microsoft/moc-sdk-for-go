c8364e6489d8a615172484437a2a7491d4842823
Neeraj Dixit <ndixit@microsoft.com>
2024-10-30 17:52:13 +0000

    change VHD get type

--- services/storage/virtualharddisk/wssd.go
+++ services/storage/virtualharddisk/wssd.go
@@ -65,7 +65,7 @@ func (c *client) CreateOrUpdate(ctx context.Context, group, container, name stri
 // Ultimately, we need the full path on disk to the disk file which we assemble from the path of the container plus the file name of the disk.
 // (e.g. "C:\ClusterStorage\Userdata_1\abc123" for the container path and "my_disk.vhd" for the disk name)
 func (c *client) Hydrate(ctx context.Context, group, container, name string, vhd *storage.VirtualHardDisk) (*storage.VirtualHardDisk, error) {
-	request, err := getVirtualHardDiskRequest(wssdcloudcommon.Operation_HYDRATE, group, container, name, vhd)
+	request, err := getVirtualHardDiskRequest(wssdcloudcommon.Operation_HYDRATE, group, container, name, vhd, "", common.ImageSource_LOCAL_SOURCE)
 	if err != nil {
 		return nil, err
 	}
