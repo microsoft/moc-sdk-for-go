cfa2d7fe59e0bc7bd6df1ccecf3866660cfe7969
wecha-ms-v2 <107636442+wecha-ms-v2@users.noreply.github.com>
2025-06-06 09:29:15 -0700

    [CMP] add support for token authentication between cloud and node agent (#324)
    * added node auth options
    
    * add unit test
    
    ---------
    
    Co-authored-by: wecha <wecha@microsoft.com>

--- services/cloud/node/node.go
+++ services/cloud/node/node.go
@@ -30,15 +30,33 @@ func getWssdNode(nd *cloud.Node, location string) (*wssdcloud.Node, error) {
 	}
 
 	if nd.AuthorizerPort == nil {
-		return nil, errors.Wrapf(errors.InvalidConfiguration, "Missing AuthorizrPort in Configuration")
+		return nil, errors.Wrapf(errors.InvalidConfiguration, "Missing AuthorizePort in Configuration")
+	}
+
+	// if node agent auth mode is not set, assume it is targetting certificate.
+	defaultNodeAgentAuthMode := cloud.NodeAgentCertificateAuth
+	if nd.NodeAgentAuthenticationMode == nil {
+		nd.NodeAgentAuthenticationMode = &defaultNodeAgentAuthMode
+	}
+
+	var nodeAgentAuthMode wssdcloud.NodeAgentAuthenticationMode
+	switch *nd.NodeAgentAuthenticationMode {
+	case cloud.NodeAgentCertificateAuth:
+		nodeAgentAuthMode = wssdcloud.NodeAgentAuthenticationMode_Certificate
+	case cloud.NodeAgentPopTokenAuth:
+		nodeAgentAuthMode = wssdcloud.NodeAgentAuthenticationMode_PopToken
+	default:
+		return nil, errors.Wrapf(errors.InvalidConfiguration, "Invalid NodeAgentAuthenticationMode %s", *nd.NodeAgentAuthenticationMode)
+
 	}
 
 	node := &wssdcloud.Node{
-		Name:           *nd.Name,
-		Fqdn:           *nd.FQDN,
-		LocationName:   location,
-		Port:           *nd.Port,
-		AuthorizerPort: *nd.AuthorizerPort,
+		Name:                        *nd.Name,
+		Fqdn:                        *nd.FQDN,
+		LocationName:                location,
+		Port:                        *nd.Port,
+		AuthorizerPort:              *nd.AuthorizerPort,
+		NodeagentAuthenticationMode: nodeAgentAuthMode,
 	}
 
 	if nd.Version != nil {
@@ -57,15 +75,26 @@ func getWssdNode(nd *cloud.Node, location string) (*wssdcloud.Node, error) {
 
 // Conversion functions from wssdcloud to cloud
 func getNode(nd *wssdcloud.Node) *cloud.Node {
+
+	nodeAgentAuthMode := cloud.NodeAgentCertificateAuth
+	switch nd.NodeagentAuthenticationMode {
+	case wssdcloud.NodeAgentAuthenticationMode_PopToken:
+		nodeAgentAuthMode = cloud.NodeAgentPopTokenAuth
+	case wssdcloud.NodeAgentAuthenticationMode_Certificate:
+	default:
+		nodeAgentAuthMode = cloud.NodeAgentCertificateAuth
+	}
+
 	return &cloud.Node{
 		Name:     &nd.Name,
 		Location: &nd.LocationName,
 		NodeProperties: &cloud.NodeProperties{
-			FQDN:           &nd.Fqdn,
-			Port:           &nd.Port,
-			AuthorizerPort: &nd.AuthorizerPort,
-			Certificate:    &nd.Certificate,
-			Statuses:       getNodeStatuses(nd),
+			FQDN:                        &nd.Fqdn,
+			Port:                        &nd.Port,
+			AuthorizerPort:              &nd.AuthorizerPort,
+			Certificate:                 &nd.Certificate,
+			Statuses:                    getNodeStatuses(nd),
+			NodeAgentAuthenticationMode: &nodeAgentAuthMode,
 		},
 		Version: &nd.Status.Version.Number,
 		Tags:    generateNodeTags(nd),
